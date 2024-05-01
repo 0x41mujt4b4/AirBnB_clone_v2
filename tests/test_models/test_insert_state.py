b28aa78f6a6b546bae6c4a8ccd8d4741e871ea0e
Almujtaba Abdalla <almujtaba007@gmail.com>
2024-05-02 01:30:16 +0300

    add test_insert_state

---
+++ tests/test_models/test_insert_state.py
@@ -0,0 +1,46 @@
+import unittest
+import mysql.connector
+import os
+
+
+host = os.getenv('HBNB_MYSQL_HOST')
+user = os.getenv('HBNB_MYSQL_USER')
+password = os.getenv('HBNB_MYSQL_PWD')
+database = os.getenv('HBNB_MYSQL_DB')
+
+class TestDatabase(unittest.TestCase):
+    """ Test the database connection """
+    def setUp(self):
+        """ Set up the database connection """
+        self.db = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database
+        )
+        self.cursor = self.db.cursor()
+    
+    def test_connection(self):
+        """ Test the database connection """
+        self.assertTrue(self.db.is_connected())
+
+    def test_insert_state(self):
+        # Get the number of records in the states table
+        self.cursor.execute("SELECT COUNT(*) FROM states")
+        count_before = self.cursor.fetchone()[0]
+        # Execute the console command (insert a new state)
+        os.system("./console.py -c 'create State name=\"California\"'")
+        # Get the number of records in the states table
+        self.cursor.execute("SELECT COUNT(*) FROM states")
+        count_after = self.cursor.fetchone()[0]
+        # Check that the number of records in the states table has increased by 1
+        self.assertEqual(count_after, count_before + 1)
+        
+
+    def tearDown(self):
+        """ Close the database connection """
+        self.cursor.close()
+        self.db.close()
+
+if __name__ == '__main__':
+    unittest.main()
